04f08c58c777e8af6b3786933b427f2c3a506505
Gabrielapsnts <Gabriela.p.snts@gmail.com>
2025-10-21 11:13:22 -0300

    Atualizandoooo

--- src/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // inicia a sessão
-session_unset(); // limpa todas as variáveis de sessão
-session_destroy(); // destrói a sessão
-header("Location: index.php"); 
-exit();
-?>
